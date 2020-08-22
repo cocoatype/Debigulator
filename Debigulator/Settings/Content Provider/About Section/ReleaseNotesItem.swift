bdd02ca221ab90d7a88d18c7f26e9933d284f3d2
Geoff Pado <geoff@pado.name>
2020-08-21 19:27:57 -0700

    Fix display of version information

--- Debigulator/Settings/Content Provider/About Section/ReleaseNotesItem.swift
+++ Debigulator/Settings/Content Provider/About Section/ReleaseNotesItem.swift
@@ -5,9 +5,17 @@ import UIKit
 
 struct ReleaseNotesItem: StandardContentItem {
     let title = NSLocalizedString("SettingsContentProvider.Item.releaseNotes", comment: "Title for the release notes settings item")
-    let subtitle: String? = "Version 20.0"
+    var subtitle: String? { String(format: Self.subtitleFormat, versionString) }
+
+    var versionString: String {
+        let infoDictionary = Bundle.main.infoDictionary
+        let versionString = infoDictionary?["CFBundleShortVersionString"] as? String
+        return versionString ?? "???"
+    }
 
     func performSelectedAction(_ sender: Any) {
         UIApplication.shared.sendAction(#selector(SettingsNavigationController.presentReleaseNotesController), to: nil, from: sender, for: nil)
     }
+
+    private static let subtitleFormat = NSLocalizedString("ReleaseNotesItem.subtitleFormat", comment: "Format string for the subtitle of the release notes settings icon")
 }
